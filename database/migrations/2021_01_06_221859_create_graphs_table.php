a713c5423a9b8cc2be009ea892e13f9a6d9a0a44
Dragos Ciobanu <dragos@cioba.nu>
2021-01-06 23:08:33 +0100

    Add graph model

---
+++ database/migrations/2021_01_06_221859_create_graphs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGraphsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('graphs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('vertices_count');
+            $table->integer('edges_count');
+            $table->jsonb('edges');
+            $table->jsonb('matrix');
+            $table->foreignId('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('graphs');
+    }
+}
